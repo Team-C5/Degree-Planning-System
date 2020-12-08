278e3366e163db6d1f0d7c1de621c7b8e6942fda
Gleici Pereira <63250461+caresdev@users.noreply.github.com>
2020-12-07 23:26:14 -0600

    Add files via upload

--- Degree_Planning_System_Project/courses.cpp
+++ Degree_Planning_System_Project/courses.cpp
@@ -14,3 +14,27 @@
 
 using namespace std;
 
+//Setter methods
+void Course::setRubric(string rubric){
+    m_rubric = rubric;
+}
+void Course::setNumber(int number){
+    m_number = number;
+}
+void Course::setName(string name){
+    m_name = name;
+}
+
+
+//Getter methods
+string Course::getRubric(){
+    return m_rubric;
+}
+int Course::getNumber(){
+    return m_number;
+}
+
+string Course::getName(){
+    return m_name;
+}
+
--- Degree_Planning_System_Project/courses.h
+++ Degree_Planning_System_Project/courses.h
@@ -12,4 +12,24 @@
 
 using namespace std;
 
+/* The class Course will instantiate course
+ objects that will produce a list of courses
+ offered by the college
+ */
 
+class Course
+{
+public:
+    void setRubric(string);
+    void setNumber(int);
+    void setName(string);
+    
+    string getRubric();
+    int getNumber();
+    string getName();
+    
+private:
+    string m_rubric;
+    int m_number;
+    string m_name;
+};
