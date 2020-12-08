ce57139940af94c24ee92d2546727da09d0e6afa
Gleici Pereira <63250461+caresdev@users.noreply.github.com>
2020-12-07 23:34:56 -0600

    Add files via upload

---
+++ Degree_Planning_System_Project/courseOfferings.cpp
@@ -0,0 +1,23 @@
+//
+//  courseOfferings.cpp
+//  Degree Planning System
+//
+//  Created by Gleici Pereira, Stephanie Organista, Jack Yu, and Keven Cruz on 12/2/20.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "courseOfferings.h"
+#include "courses.h"
+
+using namespace std;
+
+int offerings (){
+    
+    
+    
+    return 0;
+}
