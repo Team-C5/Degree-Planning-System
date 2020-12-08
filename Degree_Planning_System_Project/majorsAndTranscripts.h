c6f77ab0663ba1b1dfacf5bb352bbdc9062fe540
Stephanie Organista <55989682+StephanieOrganista@users.noreply.github.com>
2020-12-08 04:18:49 -0600

    Update majorsAndTranscripts.h
    Placed class in file

--- Degree_Planning_System_Project/majorsAndTranscripts.h
+++ Degree_Planning_System_Project/majorsAndTranscripts.h
@@ -5,9 +5,25 @@
 //  Created by Gleici Pereira, Stephanie Organista, Jack Yu, and Keven Cruz on 12/2/20.
 //
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
+#ifndef MAJORSANDTRANSCRIPTS_H
+#define MAJORSANDTRANSCRIPTS_H
+#include <string> 
 
 using namespace std;
+
+class majorsAndTranscripts
+{
+public:
+    void chosenMajor(string);
+    void courses(int);
+    void majors(int);
+    
+    string getMajor();
+    
+private:
+    string major;
+    int numberOfCourses;
+    int numberOfMajors;
+};
+
+#endif
