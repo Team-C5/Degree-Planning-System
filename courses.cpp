d4d28d05d7a9100fd1a7a14e0fdb59d2145eb67c
Gleici Pereira <63250461+caresdev@users.noreply.github.com>
2020-12-08 18:43:44 -0600

    Add files via upload

---
+++ courses.cpp
@@ -0,0 +1,51 @@
+//
+//  courses.cpp
+//  Degree Planning System
+//
+//  Created by Gleici Pereira, Stephanie Organista, Jack Yu, and Keven Cruz on 12/2/20.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+
+#include "courses.h"
+
+using namespace std;
+
+//Setter methods
+void Course::setRubric(string rubric){
+    m_rubric = rubric;
+}
+void Course::setNumber(int number){
+    m_number = number;
+}
+void Course::setName(string name){
+    m_name = name;
+}
+
+
+//Getter methods
+string Course::getRubric(){
+    return m_rubric;
+}
+int Course::getNumber(){
+    return m_number;
+}
+
+string Course::getName(){
+    return m_name;
+}
+
+
+
+// Function that displays course information
+int Course::courseOfferings(string rubric, int number, string name)
+{
+    cout << setw(8) << rubric << setw(10) << number << setw(20) << name << endl;
+
+    return 0;
+}
+
---
+++ userMenu.h
@@ -0,0 +1,19 @@
+//
+//  userMenu.h
+//  Degree Planning System
+//
+//  Created by Gleici Pereira, Stephanie Organista, Jack Yu, and Keven Cruz on 12/2/20.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int userMenu();
+
+
+
+
